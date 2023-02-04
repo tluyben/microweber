da5f4f8e944ffd3288d60fae9635d7058d47d4ca
Tycho Luyben <tycho@e-lab.nl>
2023-02-04 11:04:37 +0100

    fixed the toggle so it actually works

--- userfiles/modules/content/views/product/shipping.php
+++ userfiles/modules/content/views/product/shipping.php
@@ -5,23 +5,27 @@
 </style>
 
 <script>
-    $(document).ready(function () {
+    $(document).ready(function() {
 
-        $('.js-physical-product-check').click(function () {
+        $('.js-physical-product-check').click(function() {
             mw.toggle_physical_product_fields();
         });
 
-        <?php if ($contentData['physical_product']!=0):?>
-        mw.toggle_physical_product_fields();
-        enablePhysicalProductFields();
-        <?php else: ?>
-        disablePhysicalProductFields();
+        <?php if ($contentData['physical_product'] != 0) : ?>
+            mw.toggle_physical_product_fields();
+            enablePhysicalProductFields();
+        <?php else : ?>
+            disablePhysicalProductFields();
         <?php endif; ?>
 
     });
 
-    mw.toggle_physical_product_fields = function () {
-        $('.js-physical-product').toggle();
+    mw.toggle_physical_product_fields = function() {
+        if ($('.js-physical-product-check').prop('checked')) {
+            $('.js-physical-product').show();
+        } else {
+            $('.js-physical-product').hide();
+        }
 
         if ($('.js-physical-product-check').prop('checked')) {
             enablePhysicalProductFields();
@@ -31,13 +35,13 @@
     }
 
     function disablePhysicalProductFields() {
-        $("input,select",'.js-physical-product').prop("disabled", true);
-        $("input,select",'.js-physical-product').attr("readonly", 'readonly');
+        $("input,select", '.js-physical-product').prop("disabled", true);
+        $("input,select", '.js-physical-product').attr("readonly", 'readonly');
     }
 
     function enablePhysicalProductFields() {
-        $("input,select",'.js-physical-product').prop("disabled", false);
-        $("input,select",'.js-physical-product').removeAttr("readonly");
+        $("input,select", '.js-physical-product').prop("disabled", false);
+        $("input,select", '.js-physical-product').removeAttr("readonly");
     }
 </script>
 
@@ -51,7 +55,7 @@
             <div class="col-md-12">
                 <div class="form-group">
                     <div class="custom-control custom-checkbox">
-                        <input type="checkbox" class="custom-control-input js-physical-product-check" id="customCheck4" name="content_data[physical_product]" value="1" <?php if ($contentData['physical_product']==1):?>checked="checked"<?php endif; ?>>
+                        <input type="checkbox" class="custom-control-input js-physical-product-check" id="customCheck4" name="content_data[physical_product]" value="1" <?php if ($contentData['physical_product'] == 1) : ?>checked="checked" <?php endif; ?>>
                         <label class="custom-control-label" for="customCheck4"><?php _e("This is a physical product"); ?></label>
                     </div>
                 </div>
@@ -60,7 +64,7 @@
 
 
         <div class="js-physical-product">
-            <hr class="thin no-padding"/>
+            <hr class="thin no-padding" />
             <div class="row">
                 <div class="col-md-6">
                     <div class="form-group">
@@ -83,14 +87,14 @@
                             <div class="input-group mb-3 append-transparent">
                                 <input type="text" class="form-control" name="content_data[weight]" value="<?php echo $contentData['weight']; ?>">
                                 <div class="input-group-append">
-                            <span style="width:70px;">
-                                <select class="selectpicker" name="content_data[weight_type]" data-width="100%">
-                                    <option value="kg" <?php if ($contentData['weight_type']=='kg'):?>selected="selected"<?php endif; ?>><?php _e("kg"); ?></option>
-                                    <option value="lb" <?php if ($contentData['weight_type']=='lb'):?>selected="selected"<?php endif; ?>><?php _e("lb"); ?></option>
-                                    <option value="oz" <?php if ($contentData['weight_type']=='oz'):?>selected="selected"<?php endif; ?>><?php _e("oz"); ?></option>
-                                    <option value="g" <?php if ($contentData['weight_type']=='g'):?>selected="selected"<?php endif; ?>><?php _e("g"); ?></option>
-                                </select>
-                            </span>
+                                    <span style="width:70px;">
+                                        <select class="selectpicker" name="content_data[weight_type]" data-width="100%">
+                                            <option value="kg" <?php if ($contentData['weight_type'] == 'kg') : ?>selected="selected" <?php endif; ?>><?php _e("kg"); ?></option>
+                                            <option value="lb" <?php if ($contentData['weight_type'] == 'lb') : ?>selected="selected" <?php endif; ?>><?php _e("lb"); ?></option>
+                                            <option value="oz" <?php if ($contentData['weight_type'] == 'oz') : ?>selected="selected" <?php endif; ?>><?php _e("oz"); ?></option>
+                                            <option value="g" <?php if ($contentData['weight_type'] == 'g') : ?>selected="selected" <?php endif; ?>><?php _e("g"); ?></option>
+                                        </select>
+                                    </span>
                                 </div>
                             </div>
                         </div>
@@ -102,11 +106,11 @@
                     <div class="form-group">
                         <label class="control-label d-block mb-3"><?php _e("Free Shipping"); ?></label>
                         <div class="custom-control custom-radio d-inline-block mr-3">
-                            <input type="radio" id="customRadio1" class="custom-control-input" name="content_data[free_shipping]" value="1" <?php if ($contentData['free_shipping']==1):?>checked="checked"<?php endif; ?>>
+                            <input type="radio" id="customRadio1" class="custom-control-input" name="content_data[free_shipping]" value="1" <?php if ($contentData['free_shipping'] == 1) : ?>checked="checked" <?php endif; ?>>
                             <label class="custom-control-label" for="customRadio1"><?php _e("Yes"); ?></label>
                         </div>
                         <div class="custom-control custom-radio d-inline-block mr-3">
-                            <input type="radio" id="customRadio2" class="custom-control-input" name="content_data[free_shipping]" value="0" <?php if ($contentData['free_shipping']==0):?>checked="checked"<?php endif; ?>>
+                            <input type="radio" id="customRadio2" class="custom-control-input" name="content_data[free_shipping]" value="0" <?php if ($contentData['free_shipping'] == 0) : ?>checked="checked" <?php endif; ?>>
                             <label class="custom-control-label" for="customRadio2"><?php _e("No"); ?></label>
                         </div>
                     </div>
@@ -115,7 +119,7 @@
             <a href="javascript:;" class="btn btn-link" data-bs-toggle="collapse" data-bs-target="#advanced-weight-settings"><?php _e("Show advanced weight settings"); ?></a>
 
             <div class="collapse" id="advanced-weight-settings">
-                <hr class="thin no-padding"/>
+                <hr class="thin no-padding" />
                 <label class="control-label"><?php _e("Advanced"); ?></label>
                 <small class="text-muted d-block mb-3"><?php _e("Advanced product shipping settings."); ?></small>
 
@@ -150,7 +154,7 @@
                     <div class="col-md-12">
                         <div class="form-group">
                             <div class="custom-control custom-checkbox">
-                                <input type="checkbox" class="custom-control-input" id="show-params-checkout-page" name="content_data[params_in_checkout]" value="1" <?php if ($contentData['params_in_checkout']==1):?>checked="checked"<?php endif; ?> />
+                                <input type="checkbox" class="custom-control-input" id="show-params-checkout-page" name="content_data[params_in_checkout]" value="1" <?php if ($contentData['params_in_checkout'] == 1) : ?>checked="checked" <?php endif; ?> />
                                 <label class="custom-control-label" for="show-params-checkout-page"><?php _e("Show parameters in checkout page"); ?></label>
                             </div>
                         </div>
